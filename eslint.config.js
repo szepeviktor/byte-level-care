e4396db4e262983625dc6baf23ece41497e35054
Viktor Szépe <viktor@szepe.net>
2024-04-22 17:24:02 +0200

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,8 @@
 module.exports = [
     {
         "languageOptions": {
-            "parserOptions": {
-                "ecmaVersion": 2015,
-                "sourceType": "script"
-            }
+            "ecmaVersion": 2015,
+            "sourceType": "script"
         }
     }
 ]
